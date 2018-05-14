252e32991a151d222d99fa590bb32dad1beb497b
liukai <471091127@qq.com>
2018-05-14 15:48:28 +0800

    页面

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -3,7 +3,17 @@
     <div class="rk-container">
       <header class="rk-header">
         <div class="container">
-
+          <div class="user-info">
+            <span class="user-detail">
+              {{info.name}}
+              <img class="user-avatar" :src="info.avatar" />
+            </span>
+            <ul>
+              <li>
+                首页
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
       <main class="container">
@@ -14,7 +24,32 @@
     </div>
   </div>
 </template>
+<script>
+import api from '~/assets/api.js'
+export default {
+  asyncData ({}){
+    return api.getInfo('admin').then(res=>{
+      return {info: res.data}
+    })
+  },
+  data () {
+    return {
+      info:{
+        name:'',
+        avatar:''
+      }
+    }
+  },
+  created () {
+    api.getInfo('admin').then(res=>{
+      this.info = res.data
+    })
+  }
+}
+</script>
 <style>
+@import '~/assets/css/github.css';
+@import '~/assets/css/github-markdown.css';
   *{
     margin:0;
     padding:0;
@@ -41,5 +76,21 @@
   .rk-main{
     margin-top:80px;
     min-height:calc(100vh - 100px);
+  }
+  .user-info{
+    display:flex;
+    height:60px;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .user-detail{
+    line-height:40px;
+    font-weight:bold;
+  }
+  .user-avatar{
+    float:left;
+    margin-right:20px;
+    width:40px;
+    height:40px;
   }
 </style>
--- pages/list.vue
+++ pages/list.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="list">
-    <h2 class="subtitle">
+    <!-- <h2 class="subtitle">
       project
-    </h2>
+    </h2> -->
     <div class="article">
       <div class="article-item" v-for="(item,index) in list" :key="index">
-        <h5>{{item.title}}</h5>
-        <div class="content" v-html="item.contentHtml">
+        <div class="article-title">
+          <span>{{item.title}}</span>
+          <div class="collapse" :class="{'active':viewIndex===index}" @click.stop="changeIndex(index)">
+            <i class="top"></i>
+            <i class="bottom"></i>
+          </div>
+        </div>
+        <div v-show="viewIndex===index" class="article-content markdown-body" v-html="item.contentHtml">
 
         </div>
       </div>
@@ -24,16 +30,68 @@ export default {
   },
   data () {
     return {
-      list:[]
+      list:[],
+      viewIndex:null
     }
   },
   created(){
-    api.getArticles('admin').then(res=>{
-      return {list: res.list}
-    })
+    // api.getArticles('admin').then(res=>{
+    //   this.list=res.list
+    // })
+  },
+  methods:{
+    changeIndex(index){
+      if(this.viewIndex===index){
+        this.viewIndex=null
+        return
+      }
+      this.viewIndex=index
+    }
   }
 }
 </script>
 <style>
-
+.article-item{
+  margin-bottom:4px;
+  background-color:#fff;
+}
+.article-title{
+  position:relative;
+  padding-left:12px;
+  line-height:50px;
+  font-size:20px;
+  font-weight:bold;
+  color:#fff;
+  background:linear-gradient(to right,#04a0fb,#078dec);
+}
+.article-content{
+  padding:10px 20px;
+}
+.article-title .collapse{
+  position:absolute;
+  right:10px;
+  top:10px;
+  display:block;
+  padding:10px 0 10px 10px;
+  width:30px;
+  transition: all .2s linear;
+  cursor:pointer;
+}
+.collapse.active{
+  top:12px;
+  transform: rotate(90deg);
+}
+.collapse .top,.collapse .bottom{
+  display:block;
+  width:20px;
+  height:5px;
+  background-color:#fff;
+  border-radius: 6px;
+}
+.collapse .top{
+  transform:rotate(30deg) translateX(-4px);
+}
+.collapse .bottom{
+  transform:rotate(-30deg) translateX(-4px);
+}
 </style>
