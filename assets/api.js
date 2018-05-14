511ae95cfbc4f9e04f63659e36dcb660619fda13
liukai <471091127@qq.com>
2018-05-14 15:48:38 +0800

    前台接口

--- assets/api.js
+++ assets/api.js
@@ -2,6 +2,15 @@ import axios from 'axios'
 import qs from 'qs'
 const host = 'http://localhost:4000/'
 export default {
+  getInfo: (name) => {
+    return new Promise((resolve,reject)=>{
+      axios.post(host+'front_manage/api/getInfo',qs.stringify({name:name})).then(res=>{
+        resolve(res.data)
+      }).catch(err=>{
+        reject(err)
+      })
+    })
+  },
   getArticles: (name) => {
     return new Promise((resolve,reject)=>{
       axios.post(host+'front_manage/api/getArticles',qs.stringify({name:name})).then(res=>{
